0f1d957ffec0ae65bc53c6056eaf0b6ea7857007
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-12-07 20:10:44 -0300

    Exercício 4: Teste E2E

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,64 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Insere e edita uma tarefa', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}');
+
+    cy.contains('label', 'TP2 de ES')
+    .dblclick() 
+
+    cy.get('input.edit')
+      .clear()
+      .type('Prova de ES{enter}');
+
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+  });
+
+  it('Remove tarefa ao editar e apertar enter sem texto', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}');
+
+    cy.contains('label', 'TP2 de ES')
+      .dblclick();
+
+    cy.get('input.edit')
+      .clear()
+      .type('{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+  });
+
+  it ('Limpa lista de tarefas completas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('button.clear-completed')
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+  });
 });
